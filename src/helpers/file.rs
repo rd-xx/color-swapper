68b27a09d79f080bd25a523cb668984e6973a23b
rdx <31172722+rd-xx@users.noreply.github.com>
2023-08-08 14:07:10 +0200

    feat(file): swap colors

--- src/helpers/file.rs
+++ src/helpers/file.rs
@@ -1,11 +1,38 @@
 use std::fs;
 
 pub fn ensure_dir(path: &str) {
-    println!("ensure_dir: {}", path);
-
     fs::create_dir_all(path).expect("Couldn't create the directory");
+}
+
+pub fn clear_dir(path: &str) {
+    fs::remove_dir_all(path).expect("Couldn't clear the directory");
+    ensure_dir(path);
+}
+
+pub fn read_dir(path: &str) -> Vec<String> {
+    let entries= fs::read_dir(path).expect("Couldn't read the directory");
+    let mut files: Vec<String> = Vec::new();
+
+    for entry in entries {
+        let entry = entry.expect("Couldn't read the entry");
+        let meta = entry.metadata().expect("Couldn't read the metadata");
+
+        if meta.is_file() {
+            if let Some(v) = entry.path().to_str() {
+                files.push(v.to_string());
+            }
+        }
+    }
+
+    files = files.into_iter().filter(|x| !x.contains(".DS_Store")).collect();
+
+    return files;
 }
 
 pub fn read_file(path: &str) -> String {
     fs::read_to_string(path).expect("Couldn't read the file")
+}
+
+pub fn write_file(path: &str, content: &str) {
+    fs::write(path, content).expect("Couldn't write the file");
 }
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,26 @@ mod helpers {
 }
 
 fn main() {
-    println!("Hello, world!");
-    helpers::file::ensure_dir("test");
-
     let config = helpers::config::read_config();
-    println!("config: {}", config.input_folder);
+
+    helpers::file::ensure_dir(&config.input_folder);
+    helpers::file::ensure_dir(&config.output_folder);
+    helpers::file::clear_dir(&config.output_folder);
+
+    let input_paths = helpers::file::read_dir(&config.input_folder);
+    for path in input_paths {
+        let mut file_content = helpers::file::read_file(&path);
+
+        for color in &config.colors {
+            let from = &color.from;
+            let to = &color.to;
+
+            file_content = file_content.replace(from, to);
+        }
+
+        let output_path = path.replace(&config.input_folder, &config.output_folder);
+        helpers::file::write_file(&output_path, &file_content);
+
+        println!("File {} done.", path.split("/").last().unwrap());
+    }
 }
